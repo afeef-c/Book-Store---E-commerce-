e25b05d5baa03e1760368c7d922f37ce5a0c643a
afeef-c <afeefc123@gmail.com>
2025-01-10 08:59:32 +0530

    first commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@ import { logoutUser, setAuthTokens } from "../features/authSlice";
 
 
 const api = axios.create({
-    baseURL: 'import.meta.env.VITE_API_URL'
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 let isRefreshing = false;
